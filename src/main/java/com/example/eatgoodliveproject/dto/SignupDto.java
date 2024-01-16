f7b66d04fb2a391e475943724720e00d75130b29
ofektom <okpohot@gmail.com>
2024-01-16 02:34:39 +0100

    adds spring security configurations

--- src/main/java/com/example/eatgoodliveproject/dto/SignupDto.java
+++ src/main/java/com/example/eatgoodliveproject/dto/SignupDto.java
@@ -1,12 +1,20 @@
 package com.example.eatgoodliveproject.dto;
 
 import com.example.eatgoodliveproject.enums.CountryCode;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class SignupDto {
     private String fullName;
-    private String email;
-    private CountryCode countryCode;
-    private Long phoneNumber;
-    private String Password;
+    private String username;
+    private String countryCode;
+    private String phoneNumber;
+    private String password;
     private String confirmPassword;
 }
--- src/main/java/com/example/eatgoodliveproject/model/Address.java
+++ src/main/java/com/example/eatgoodliveproject/model/Address.java
@@ -1,13 +1,24 @@
 package com.example.eatgoodliveproject.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-
+@Entity
 public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    @ManyToMany(mappedBy = "addresses")
+    private List<Users> users = new ArrayList<>();
 }
